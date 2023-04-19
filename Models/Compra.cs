f77bd2f3b644636aa5c6948bb6620ca8426fb64f
DAVID MONTES <92450281+DAVIDMIRANDAD@users.noreply.github.com>
2023-04-18 19:50:31 -0500

    Prueba

--- Models/Compra.cs
+++ Models/Compra.cs
@@ -10,4 +10,6 @@ namespace RestauranteKarol.Models
         public DateTime FechaDespacho { get; set; }
         public string TipoCompra { get; set; } = null!;
     }
+
+    System.out.println();
 }
